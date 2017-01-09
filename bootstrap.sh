985de4c6f8216c9d3ff8b4d515ecf6032f188936
Chris McCluskey <mcclusk@gmail.com>
2017-01-09 05:54:44 +0000

    Init of the misc repo

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Debian/Ubuntu
+if [ -e  /etc/network/interfaces ]; then
+  sudo apt-add-repository multiverse
+  sudo apt-get update
+  echo 'ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true' | sudo debconf-set-selections
+  sudo apt-get install -y ubuntu-restricted-extras
+  sudo apt-get install -y vim
+  sudo apt-get install -y libdvdread4
+  sudo /usr/share/doc/libdvdread4/install-css.sh
+  sudo /sbin/ldconfig
+  sudo apt-get install -y vlc
+  sudo apt-get install -y git
+  sudo apt-get install -y ruby 
+
+###################
+# Redhat/Centos
+elif [ -e /etc/redhat-release ]; then
+  sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
+  sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+  #sudo rpm -Uvh http://pkgs.repoforge.org/rpmfor@scge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm 
+  sudo rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+  sudo yum install -y libdvdread libdvdcss
+  sudo yum install -y vlc
+  #sudo yum install --disableplugin=fastestmirror -y xv
+  #sudo yum install --disableplugin=fastestmirror -y geeqie
+  ##sudo yum install --enable-repo=rpmforge-extras -y bchunk 
+  #sudo yum install --disableplugin=fastestmirror -y /lib/ld-linux.so.2
+  #sudo yum install --disableplugin=fastestmirror -y glibc glibc-common glibc-utils
+  #sudo rpm -Uvh http://he.fi/bchunk/bchunk-1.2.0-0.i386.rpm
+  #sudo yum install --disableplugin=fastestmirror -y unrar
+  #sudo yum install --disableplugin=fastestmirror -y p7zip
+fi
