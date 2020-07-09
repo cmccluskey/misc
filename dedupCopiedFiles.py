ee00232496bb4a9459a977a58b83f939de6ee2d6
Chris McCluskey <mcclusk@gmail.com>
2020-07-09 02:21:00 +0000

    WIP

--- dedupCopiedFiles.py
+++ dedupCopiedFiles.py
@@ -1,4 +1,101 @@
-#!/opt/local/bin/python3
+#!/usr/bin/python3
+
+import argparse
+import errno
+import hashlib
+import logging
+import os
+import pprint
+import re
+import shutil 
+import sys
+
+
+# Move to commmon
+def build_file_tree(items=[], recurse=False):
+  temp_dict = {}
+  for item in args.items:
+    if os.path.isfile(item):
+      temp_dict[os.path.abspath(item)] = item 
+    if item == '*':
+      item = os.getcwd()
+    if os.path.isdir(item):
+      if recurse:
+        for root, dirs, files in os.walk(item):
+          for f in files:
+            temp_dict[os.path.abspath(os.path.join(root,f))] = item
+      else:
+        for possiblefile in os.listdir(item):
+          if os.path.isfile(os.path.join(item, possiblefile)):
+            temp_dict[os.path.abspath(os.path.join(item, possiblefile))] = item
+  return sorted(temp_dict.keys())
+
+
+#def mkdir_p(path):
+#  try:
+#    os.makedirs(path)
+#  except OSError as exc: 
+#    if exc.errno == errno.EEXIST and os.path.isdir(path):
+#      pass
+#    else:
+#      raise
+
+
+def cksum(filename):
+  BLOCKSIZE = 65536
+  hasher = hashlib.sha1()
+  try:
+    with open(filename, 'rb') as afile:
+      buf = afile.read(BLOCKSIZE)
+      while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(BLOCKSIZE)
+  except:
+    return None
+  return hasher.hexdigest()
+
+
+# Globals
+
+# Parser setup
+parser = argparse.ArgumentParser(description='Looks for various files from copy processes and remove/rewrite the filename based on the results.')
+
+parser.add_argument('-a', dest='aggressive', action='store_true', default=False, help='For items that can have an incremented value, search for duplicates before move/delete.')
+parser.add_argument('-noffend', dest='unixoffend', action='store_false', default=True, help='Remove typically offending Unix shell characters.')
+parser.add_argument('-R', dest='recurse', action='store_true', default=False, help='Recurse into directories.')
+parser.add_argument('-d', dest='debug', action='store_true', default=False, help='Enable debugging to standard out.')
+parser.add_argument('-r', dest='report', action='store_true', default=False, help='Report only, do not touch files.')
+parser.add_argument('-nlog', dest='actionlog', action='store_const', const=None, default='action.log', help='Disable action log generation.')
+parser.add_argument('-ntrash', dest='trash', action='store_const', const=None, default='.Trash',  help='Disable use of Trash folder.')
+parser.add_argument('items', nargs='+', default=None)
+
+args = parser.parse_args()
+
+
+# Enable logger
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+if (args.debug):
+   logger.setLevel(logging.DEBUG)
+else:
+   logger.setLevel(logging.ERROR)
+#for lh in (""):
+#   logging.getLogger(lh).setLevel(100)
+
+
+# Test write of actionlog
+if args.actionlog:
+  try:
+    af = open(args.actionlog, 'a')
+  except IOError as e:
+    logger.error("Cannot open action file %s for writing" % args.actionlog)
+    os.exit(1)
+
+
+files = build_file_tree(args.items, args.recurse)
+
+print(files)
+
 
 #TODO
 #Add aggressive checking of serial items
@@ -30,74 +127,24 @@
 
 # Unix offending character sets [`,@,&,;,%,!,]
 
-import argparse
-import errno
-import hashlib
-import os
-import pprint
-import re
-import shutil 
-import sys
+checksum_cache = {}
 
+for item in files:
+  print(item)
+  original = item
+  (newfile, newsuffix) = os.path.splitext(os.path.basename(item))
+  newbasedir = os.path.dirname(item)
+  checksum_cache[original] = cksum(original)
+  if args.debug:
+    logger.debug("Original: %s" % original)
+    logger.debug("Original Sum: %s" % checksum_cache[original])
+    logger.debug("New File: %s" % newfile )
+    logger.debug("New Suffix: %s" % newsuffix)
+    logger.debug("New Base Dir: %s" % newbasedir)
+  deleted = False
 
-def build_file_tree(items=[], recurse)
-  temp_dict = {}
-  for item in args.items:
-    if item == '*':
-      if len(args.items) > 1:
-        print("Error: Cannot mix an escaped * and other arguments")
-        sys.exit(1)
-      else:
-        args.items=[]
-        for root, dirs, files in os.walk(os.getcwd()):
-          for f in files:
-            args.items.append(os.path.join(root,f))
-
-
-#def mkdir_p(path):
-#  try:
-#    os.makedirs(path)
-#  except OSError as exc: 
-#    if exc.errno == errno.EEXIST and os.path.isdir(path):
-#      pass
-#    else:
-#      raise
-
-def cksum(filename):
-  BLOCKSIZE = 65536
-  hasher = hashlib.sha1()
-  try:
-    with open(filename, 'rb') as afile:
-      buf = afile.read(BLOCKSIZE)
-      while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(BLOCKSIZE)
-  except:
-    return None
-  return hasher.hexdigest()
-
-# Globals
-
-# Parser setup
-parser = argparse.ArgumentParser(description='Looks for various files from copy processes and remove/rewrite the filename based on the results.')
-
-parser.add_argument('-a', dest='aggressive', action='store_true', default=False, help='For items that can have an incremented value, search for duplicates before move/delete')
-parser.add_argument('-o', dest='unixoffend', action='store_true', default=False, help='Remove typically offending Unix shell characters')
-parser.add_argument('-R', dest='recurse', action='store_true', default=False, help='Recurse into directories')
-parser.add_argument('-d', dest='debug', action='store_true', default=False, help='Enable debugging to standard out.')
-parser.add_argument('-r', dest='report', action='store_true', default=False, help='Report only, do not touch files.')
-parser.add_argument('items', nargs='+', default=None)
-
-args = parser.parse_args()
-
-
-#for item in args.items:
-#  original = item
-#  original_sum = cksum(item)
-#  if args.debug:
-#    print("Original: %s" % original )
-#    print("Original: %s" % original_sum )
-#  deleted = False
+#  if not args.noffend:
+    
 #
 #  # .Id_NN 
 #  re_Id = re.compile(r'\.Id_[0-9]+\.')
@@ -288,6 +335,4 @@ args = parser.parse_args()
 #          continue
 #    else:
 #      done = True
-#
-#print("DONE.")
 #
